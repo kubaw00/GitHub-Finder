4e3a5bea0323dd004b41e42cb2ff7698b43a38fc
Jakub Widurek <jakubwidurek@gmail.com>
2021-09-07 20:32:18 +0200

    add files

--- github.js
+++ github.js
@@ -11,7 +11,7 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
     const repoResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=${this.repos_coun}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
     const profile = await profileResponse.json();
